6038a16dd1e13f3d3309af12558e612f33e0bd17
07Kaustubh <101243527+07Kaustubh@users.noreply.github.com>
2025-07-07 05:30:39 +0000

    cls

--- app.py
+++
@@ -1,9 +0,0 @@
-from modules.main_app import MainApp
-from PyQt5 import QtWidgets
-import sys
-
-if __name__ == "__main__":
-    app = QtWidgets.QApplication(sys.argv)
-    window = MainApp()
-    window.showMaximized()
-    sys.exit(app.exec_())
